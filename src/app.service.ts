f883418af008c811c161d558c01b4321053d4cbc
haroldCoder <69125414+haroldCoder@users.noreply.github.com>
2024-11-14 02:17:20 +0000

    close connection with db when server ocurred an error o is closed

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Pool } from 'mysql2/promise';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('DATABASE_CONNECTION') private readonly pool: Pool) {
+    process.on("SIGINT", ()=>{
+      if(pool){
+        pool.end()
+      }
+    });
+    process.on("SIGTERM", ()=>{
+      if(pool){
+        pool.end()
+      }
+    });
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
