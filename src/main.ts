0265d74af93a72c93f898ebb80ed2a7f96bf064d
haroldCoder <69125414+haroldCoder@users.noreply.github.com>
2024-11-29 02:10:07 +0000

    make connection and implement with redis

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Redisadp } from './servers/redisadp/redisadp';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const redisIoAdapter = new Redisadp(app);
+  await redisIoAdapter.connectRedis()
+  app.useWebSocketAdapter(redisIoAdapter);
   app.enableCors({origin: true})
   await app.listen(process.env.PORT ?? 3000);
 }
